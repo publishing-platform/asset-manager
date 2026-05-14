441a7c56b6b29d159a0196adf84f7d94ac7e3586
neilha <neilharrold@gmail.com>
2026-05-14 15:34:59 +0100

    add fake S3 route and corresponding routing tests

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get "/media/:id/:filename" => "media#download", :constraints => { filename: /.*/ }, as: :download_media
 
+  if AssetManager.s3.fake?
+    mount Rack::Files.new(AssetManager.fake_s3.root), at: AssetManager.fake_s3.path_prefix, as: "fake_s3"
+  end
+
   get "/healthcheck/live", to: proc { [200, {}, %w[OK]] }
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
---
+++ spec/routing/fake_s3_route_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe "routes for S3Storage::Fake", type: :routing do
+  let(:fake_s3_route) { Rails.application.routes.named_routes.get("fake_s3") }
+  let(:s3_config) { instance_double(S3Configuration, fake?: s3_fake_enabled) }
+
+  before do
+    allow(AssetManager).to receive(:s3).and_return(s3_config)
+    Rails.application.reload_routes!
+  end
+
+  after do
+    allow(AssetManager).to receive(:s3).and_call_original
+    Rails.application.reload_routes!
+  end
+
+  context "when fake S3 is enabled" do
+    let(:s3_fake_enabled) { true }
+
+    it "sets up fake_s3 route" do
+      expect(fake_s3_route).to be_present
+    end
+  end
+
+  context "when fake S3 is not enabled" do
+    let(:s3_fake_enabled) { false }
+
+    it "does not set up fake_s3 route" do
+      expect(fake_s3_route).not_to be_present
+    end
+  end
+end
