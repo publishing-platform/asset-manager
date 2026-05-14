4566a90c8d3ba763276a3657db899add4efd503e
neilha <neilharrold@gmail.com>
2026-05-14 15:30:55 +0100

    implements and tests SaveToCloudStorageJob

--- app/sidekiq/save_to_cloud_storage_job.rb
+++ app/sidekiq/save_to_cloud_storage_job.rb
@@ -1,7 +1,13 @@
-# require "services"
+require "services"
 
 class SaveToCloudStorageJob
   include Sidekiq::Job
 
-  def perform(asset_id); end
+  def perform(asset_id)
+    asset = Asset.undeleted.find(asset_id)
+    unless asset.uploaded?
+      Services.cloud_storage.upload(asset)
+      asset.upload_success!
+    end
+  end
 end
---
+++ spec/sidekiq/save_to_cloud_storage_job_spec.rb
@@ -0,0 +1,60 @@
+require "rails_helper"
+
+RSpec.describe SaveToCloudStorageJob, type: :worker do
+  let(:worker) { described_class.new }
+
+  describe "#perform" do
+    let(:asset) { create(:clean_asset) }
+    let(:cloud_storage) { instance_double(S3Storage) }
+
+    before do
+      allow(Services).to receive(:cloud_storage).and_return(cloud_storage)
+      allow(cloud_storage).to receive(:upload)
+    end
+
+    it "saves the asset to cloud storage" do
+      expect(cloud_storage).to receive(:upload).with(asset)
+
+      worker.perform(asset.id)
+    end
+
+    it "changes the state of the asset to uploaded" do
+      worker.perform(asset.id)
+
+      expect(asset.reload).to be_uploaded
+    end
+
+    context "when asset is already uploaded" do
+      let(:asset) { create(:uploaded_asset) }
+
+      it "does not save the asset to cloud storage" do
+        expect(cloud_storage).not_to receive(:upload).with(asset)
+
+        worker.perform(asset.id)
+      end
+
+      it "does not change the state of the asset" do
+        worker.perform(asset.id)
+
+        expect(asset.reload).to be_uploaded
+      end
+    end
+
+    context "when S3Storage::ObjectUploadFailedError is raised" do
+      before do
+        allow(cloud_storage).to receive(:upload)
+          .and_raise(S3Storage::ObjectUploadFailedError)
+      end
+
+      it "leaves the state of the asset set to clean" do
+        begin
+          worker.perform(asset.id)
+        rescue StandardError
+          nil
+        end
+
+        expect(asset.reload).to be_clean
+      end
+    end
+  end
+end
