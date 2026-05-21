6d64e9874d459a5dd09c0f274b1a87116ed36ea8
neilha <neilharrold@gmail.com>
2026-05-21 16:20:25 +0100

    add AssetProcessor class and Rake task for deleting assets from NFS

---
+++ lib/asset_processor.rb
@@ -0,0 +1,25 @@
+class AssetProcessor
+  def initialize(scope: Asset, output: $stdout, report_progress_every: 1000)
+    @scope = scope
+    @output = output
+    @report_progress_every = report_progress_every
+  end
+
+  def process_all_assets_with
+    @output.sync = true
+    asset_ids = @scope.pluck(:id).to_a
+    total = asset_ids.count
+    asset_ids.each_with_index do |asset_id, index|
+      count = index + 1
+      percent = (count / total.to_f * 100).round
+      if (count % @report_progress_every).zero?
+        @output.puts "#{count} of #{total} (#{percent}%) assets"
+      end
+      yield asset_id
+    end
+    unless (total % @report_progress_every).zero?
+      @output.puts "#{total} of #{total} (100%) assets"
+    end
+    @output.puts "\nFinished!"
+  end
+end
